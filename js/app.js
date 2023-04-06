269ea9e38f17ff576468b8386412a501c75e1fb1
MohsenEMX <zmc.site@gmail.com>
2023-04-06 14:55:43 +0330

    C

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 const startB = document.querySelector("#startB");
 const NextB = document.querySelector("#NextB");
+
 const centerDiv = document.querySelector(".center");
 const titledDiv = document.querySelector(".titled");
-const br1Div = document.querySelector(".br1");
-
 const gameDiv = document.querySelector(".game");
+
 const out1 = document.querySelector("#out1");
 const out2 = document.querySelector("#out2");
 
@@ -15,7 +15,6 @@ let buttonCkeckL = true;
 let gameWord = "";
 let spies = [];
 let currentPlayer = 1;
-let functionTurn = "game";
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
@@ -30,7 +29,6 @@ startB.addEventListener("click", function () {
 function draw() {
   centerDiv.style.display = "none";
   titledDiv.style.display = "none";
-  br1Div.style.display = "none";
   gameDiv.style.display = "block";
 
   initg();
@@ -51,37 +49,25 @@ function initg() {
     console.log("spies: " + spies);
   }
 
-  buttonCkeck();
+  game();
 }
 
-function buttonCkeck() {
-  NextB.addEventListener("click", function () {
-    if (functionTurn == "game") game();
-    else if (functionTurn == "wordDisp") wordDisp();
-  });
-  // if (buttonCkeckL) buttonCkeck();
-}
 function game() {
   out1.innerHTML = "It's Player " + currentPlayer + "'s Turn";
   if (currentPlayer > numOfPlayers) {
     last(totalMinutes);
   } else {
-    wordDisp();
+    if (!spies.includes(currentPlayer)) {
+      out1.innerHTML = "You are the Spy";
+    } else {
+      out1.innerHTML =
+        "Your Word is:  " + '<span class="pers">' + gameWord + "</span>";
+    }
+
     currentPlayer += 1;
   }
-  functionTurn = "wordDisp";
 }
 
-function wordDisp() {
-  if (!spies.includes(currentPlayer)) {
-    out1.innerHTML = "You are the Spy";
-  } else {
-    out1.innerHTML =
-      "Your Word is:  " + '<span class="pers">' + gameWord + "</span>";
-  }
-
-  functionTurn = "game";
-}
 function last(totalMinutes) {
   gameDiv.style.display = "none";
   let sec = totalMinutes * 60;
